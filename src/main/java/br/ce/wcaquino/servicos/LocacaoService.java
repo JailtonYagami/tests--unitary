df1b466dd4b64e77db4799214cee46f30d96e8cb
jailton gilberto <jailtonryuji1@gmail.com>
2022-08-29 19:18:58 -0300

    "Trabalhando com Assertivas"

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,45 +1,30 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-
-import java.util.Date;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
 public class LocacaoService {
-	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
-		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
-		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
 
-		//Entrega no dia seguinte
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 1);
-		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
-		return locacao;
-	}
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
 
-	public static void main(String[] args) {
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario =  new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1",2,5.0);
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
 
-		//acao
-		Locacao locacao = service.alugarFilme(usuario,filme);
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
 
-		//verificação
-		System.out.println(locacao.getValor());
-		System.out.println(locacao.getDataLocacao());
-		System.out.println(locacao.getDataRetorno());
-	}
+        return locacao;
+    }
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,53 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+
+        Assert.assertEquals(1,1);
+        Assert.assertEquals("Erro de comparação",1,2);
+        Assert.assertEquals(0.51234,0.512,0.001);
+        Assert.assertEquals(Math.PI,3.14,0.01);
+
+        int i = 5;
+        Integer i2 = 5;
+
+
+        Assert.assertEquals(Integer.valueOf(i),i2);
+        Assert.assertEquals(i,i2.intValue());
+
+
+        Assert.assertEquals("bola","bola");
+        Assert.assertNotEquals("bola","casa");
+
+        Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+        Assert.assertTrue("bola".startsWith("bo"));
+
+        Usuario u1 = new Usuario("Usario 1");
+        Usuario u2 = new Usuario("Usario 1");
+        Usuario u3 = null;
+
+
+        Assert.assertEquals(u2,u2);
+
+        //garantir que os objetos sejam da mesma instância, caso aponte para mesma instância
+        Assert.assertSame(u2,u2);
+
+        //garantir que apesar de serem iguais são de instâncias diferentes
+        Assert.assertNotSame(u1,u2);
+
+        //verificar se é nulo
+        Assert.assertTrue(u3 == null);
+
+        Assert.assertNull(u3);
+        Assert.assertNotNull(u2);
+
+    }
+}
